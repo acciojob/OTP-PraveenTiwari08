8aa621ed93c39cdae8c3bb6afbf734353e440333
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-04-15 23:34:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 //your JS code here. If required.
-function focusNextInput(currentInput) {
-        if (currentInput.value.length === 1) {
-            var nextInput = currentInput.nextElementSibling;
-            if (nextInput) {
-                nextInput.focus();
-            }
-        } else if (currentInput.value.length === 0) {
-            var previousInput = currentInput.previousElementSibling;
-            if (previousInput) {
-                previousInput.focus();
-            }
-        }
-    }
