7b4cba00ab0a9f0d959192b889d60f9a9d7d87cb
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-04-30 01:57:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function focusNextInput(currentInput) {
         nextInput.focus();
     }
 }
+// Typing the OTP digits into the input fields
+cy.visit(baseUrl + "/main.html");
+cy.get(".code").eq(0).type(5); // Input first digit
+cy.get(".code").eq(1).type(1); // Input second digit
+cy.get(".code").eq(2).type(7); // Input third digit
+cy.get(".code").eq(3).type(2); // Input fourth digit
+cy.get(".code").eq(4).type(9); // Input fifth digit
+cy.get(".code").eq(5).type(6); // Input sixth digit
