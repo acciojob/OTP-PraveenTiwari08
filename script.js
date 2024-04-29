5674143674ebff9922874a5ed7d8334da7944ab3
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-04-30 01:59:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ cy.get(".code").eq(2).type(7); // Input third digit
 cy.get(".code").eq(3).type(2); // Input fourth digit
 cy.get(".code").eq(4).type(9); // Input fifth digit
 cy.get(".code").eq(5).type(6); // Input sixth digit
+
+// Backspacing to clear the OTP digits
+cy.visit(baseUrl + "/main.html");
+cy.get(".code").eq(5).type("{backspace}"); // Backspace last digit
+cy.get(".code").eq(4).type("{backspace}"); // Backspace fifth digit
+cy.get(".code").eq(3).type("{backspace}"); // Backspace fourth digit
+cy.get(".code").eq(2).type("{backspace}"); // Backspace third digit
+cy.get(".code").eq(1).type("{backspace}"); // Backspace second digit
+cy.get(".code").eq(0).type("{backspace}"); // Backspace first digit
